d03d3f8344597d2abb47c747a02be8025215b55e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-25 20:34:24 +0000

    Deploy maccoda/ts-trivia to github.com/maccoda/ts-trivia.git:gh-pages

---
+++ precache-manifest.85397fc4ad970fa5ed1caed792d01683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58ee5fbcd341248caf3805633896d05",
+    "url": "/ts-trivia/index.html"
+  },
+  {
+    "revision": "68237ca72a23ed7977a0",
+    "url": "/ts-trivia/static/css/main.7914c8fe.chunk.css"
+  },
+  {
+    "revision": "6408e9f36b120fa1de66",
+    "url": "/ts-trivia/static/js/2.488002a2.chunk.js"
+  },
+  {
+    "revision": "3106e5074ad29089816fb2b6dbb3d46d",
+    "url": "/ts-trivia/static/js/2.488002a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68237ca72a23ed7977a0",
+    "url": "/ts-trivia/static/js/main.e3dd337c.chunk.js"
+  },
+  {
+    "revision": "dbe888cb63502353a805",
+    "url": "/ts-trivia/static/js/runtime-main.e517a0a5.js"
+  }
+]);
